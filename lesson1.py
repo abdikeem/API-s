a4ac8f05c08a94372b3632490987a4c0c422c966
abdikeem <abdihakimadbirahman21@gmail.com>
2022-06-13 16:25:55 +0300

    lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Welcome to my FastAPi"}
+
+@app.get("/lesson2")
+def lesson2():
+    return {"Data": "Welcome to my FastAPi"}
+
+@app.post("/createpost")
+def createPost(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
